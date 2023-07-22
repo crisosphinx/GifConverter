d4e43ebb9f9be8b87099b47a203fbf835557d42f
Jeffrey Miller <jeff3danimation@yahoo.com>
2023-07-22 01:24:18 -0700

    Code clean up, pylint, tasks, version

---
+++ __version__.py
@@ -0,0 +1,47 @@
+""" Version file with extra data to help with accreditation """
+VERSION = "1.0.0"
+
+TITLE = rf"""
+ _         _____  ___    ___       _____  _____     ___    _  ___   
+(_)/'\_/`\(  _  )(  _`\ (  _`\    (_   _)(  _  )   (  _`\ (_)(  _`\ 
+| ||     || (_) || ( (_)| (_(_)     | |  | ( ) |   | ( (_)| || (_(_)
+| || (_) ||  _  || |___ |  _)_      | |  | | | |   | |___ | ||  _)  
+| || | | || | | || (_, )| (_( )     | |  | (_) |   | (_, )| || |    
+(_)(_) (_)(_) (_)(____/'(____/'     (_)  (_____)   (____/'(_)(_)
+
+VERSION {VERSION}
+"""
+
+CREDITS = """
+Thank you for using this software.
+
+ImageToGif was created to turn an
+image sequence into a usable gif.
+
+Any modifications to the software
+in the future can be provided or
+requested on the github of:
+
+https://github.com/crisosphinx/GifConverter
+
+
+Copyright (c) 2023.
+
+
+DISCLAIMER:
+
+Jeff Miller (Jeff3DAnimation.com) assumes
+no responsibility or liability for any
+bugs, errors or destruction of files by
+using this program.
+
+The software contained is provided on an
+"as is" basis with no guarantees of
+full completeness, accuracy, usefulness
+or timeliness...
+
+If a bug does occur, please contact me
+(Jeff Miller) at jeff3danimation@gmail.com.
+
+
+"""
--- tasks.py
+++ tasks.py
@@ -1,10 +1,28 @@
+""" Tasks file for making things easier to test """
 import shutil
 from pathlib import Path
 from invoke.tasks import task
 
 
 @task()
+def lint(ctx):
+    """
+    Linting step.
+
+    :param ctx: Context
+    :type ctx: Context
+    """
+    ctx.run('pylint *')
+
+
+@task()
 def build(ctx):
+    """
+    Build step.
+
+    :param ctx: Context
+    :type ctx: Context
+    """
     ctx.run('pyinstaller -F --uac-admin --icon="./resources/ToGif.ico" --clean main.py')
     dist_dir = Path('dist')
     dist_file = dist_dir.joinpath('main.exe')
